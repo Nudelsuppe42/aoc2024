6545898eef908525d199aec942d705afbedf3183
Nudesuppe42 <jufo.theo@gmail.com>
2024-12-07 20:11:33 +0100

    feat(day1): :sparkles: Finish day1

--- days/day1.ts
+++ days/day1.ts
@@ -15,5 +15,13 @@ export default function day1(input: string) {
         distance += Math.abs(array1[element] - array2[element])
     }
 
-    return distance+""
+    let similarity = 0;
+
+    for (const element in array1) {
+        const count = array2.filter(e => e == array1[element]).length;
+
+        similarity += count* array1[element]
+    }
+
+    return distance+"\n"+similarity
 }
